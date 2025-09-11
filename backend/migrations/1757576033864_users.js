961c560a7168847fdd1af3be39f288b36efdd601
Everrynn✨ <20amigo04@gmail.com>
2025-09-11 12:44:38 +0500

    Create users migration

---
+++ backend/migrations/1757576033864_users.js
@@ -0,0 +1,15 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: "id",
+    email: { type: "varchar(100)", notNull: true, unique: true },
+    password: { type: "varchar(255)", notNull: true },
+    avatar: { type: "varchar(255)", notNull: false },
+    created_at: { type: "timestamp", default: pgm.func("current_timestamp") },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("users");
+};
